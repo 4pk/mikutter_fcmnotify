b8a89cb6fd178f07af8f8236598b08e3e6c05a4d
4pk <mikutter4pk@gmail.com>
2018-08-03 00:59:20 +0900

    test

---
+++ mikutter_fcmnotify.rb
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+require '~/.mikutter/plugin/fcm/fcm.rb'
+
+Plugin.create(:mikutter_fcmnotify) do
+
+ on_favorite do |service, user, msg|
+    if msg.from_me?
+      data = {
+       :title => "Favorited by #{user.idname}",
+       :body => msg.description,
+       :url => 'https://mikutter.hachune.net/'
+    }
+      Plugin.call(:fcm, data)
+    end
+  end 
+
+  on_mention do |service ,msg|
+    msg.each do |m|
+      if Time.now - m.created < 10 and !m.retweet?
+        data = {
+        :title => "Mentioned by #{m.user.idname}",
+        :body => m.description,
+        :url => 'https://mikutter.hachune.net/'
+      }
+       Plugin.call(:fcm, data)
+      end
+    end
+  end
+
+  on_retweet do |msg|
+    msg.each do |m|
+      if Time.now - m.created < 10
+        m.retweet_source_d.next { |s|
+          if s.from_me?
+            data = {
+            :title => "ReTweeted by #{m.user.idname}",
+            :body => s.description,
+            :url => 'https://mikutter.hachune.net/'
+          }
+           Plugin.call(:fcm, data)
+          end
+        }
+      end
+    end
+  end
+
+end
